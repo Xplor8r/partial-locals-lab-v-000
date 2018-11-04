acd033d48c33da152edc89e285a639c0d8e4e493
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-04 03:05:19 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -22,7 +22,7 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.search(params[:query])
-    rendr 'index'
+    render 'index'
   end
 
   def student_params
